858d0f6b5bfa9ac609e139578da545623f2e495c
nezrinabbaszade2@gmail.com <nezrinabbaszade2@gmail.com>
2022-07-01 23:24:51 +0400

    21

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello 1");
+            Console.WriteLine("Hello n");
 
         }
     }
